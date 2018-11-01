4c7964b56df8d6568976098631629de1e91c7b27
odipojames <odipojames12@gmai.com>
2018-11-01 22:44:53 +0300

    add save user test and tear down

--- password_test.py
+++ password_test.py
@@ -22,6 +22,18 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"james")
         self.assertEqual(self.new_user.password,"aoko122")
 
+    def test_save_user(self):
+        '''
+        test to check whether user information is being saved
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
+
+    def tearDown(self):
+        '''
+        cleans up after each test runs
+        '''
+        User.users_list = [] 
 
 if __name__ == '__main__':
     unittest.main()
